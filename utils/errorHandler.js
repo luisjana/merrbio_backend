394c47a030e29f40d236a176ad1bbe5d6735aaf0
luisjana <luisjanamuslika@gmail.com>
2025-05-23 10:04:37 +0200

    �Fix: Ensure express-validator is included for Render deployment

--- utils/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const handleError = (res, error, message = 'Server error') => {
-    console.error(error);
-    res.status(500).json({ message, error: error.message });
-  };
-  
-  module.exports = handleError;
-  
